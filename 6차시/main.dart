55d3dab6f4e7938efee6ca3e4b563545bc22bc7a
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-05 22:17:18 +0900

    Create main.dart

---
+++ 6차시/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'main_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '디데이',
+      home: MainPage(),
+    );
+  }
+}
